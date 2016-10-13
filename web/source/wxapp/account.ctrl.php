920ead8fe9643b05f20738c1b2d74fe343ef76e6
RenChao <donknap@qq.com>
2016-10-13 14:00:30 +0800

    跳转管理小程序

--- web/source/wxapp/account.ctrl.php
+++ web/source/wxapp/account.ctrl.php
@@ -6,7 +6,7 @@
 defined('IN_IA') or exit('Access Denied');
 $_W['page']['title'] = '小程序列表 - 帐号';
 
-$dos = array('display');
+$dos = array('display', 'switch');
 $do = in_array($do, $dos) ? $do : 'display';
 
 if ($do == 'display') {
@@ -38,4 +38,12 @@ if ($do == 'display') {
 		)
 	);
 	template('wxapp/account-display');
+} elseif ($do == 'switch') {
+	$uniacid = intval($_GPC['uniacid']);
+	$version = pdo_get('wxapp_versions', array('uniacid' => $uniacid), array('version', 'multiid'), 'version DESC');
+	
+	isetcookie('__uniacid', $uniacid, 7 * 86400);
+	isetcookie('__uid', $_W['uid'], 7 * 86400);
+	header('Location: ' . url('wxapp/manage/edit', array('multiid' => $version['multiid'])));
+	exit;
 }
