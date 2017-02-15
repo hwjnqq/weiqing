8d4aaa83fbbf1f20a47319d3816a54d096968eca
yangfeng <117635341@qq.com>
2017-02-15 16:27:56 +0800

    如果表里content是mediumtext则不执行更新脚本里的sql语句

--- addons/we7_business/upgrade.php
+++ addons/we7_business/upgrade.php
@@ -1,8 +1,9 @@
 <?php
 //字段长度
-if(pdo_fieldexists('business', 'content')) {
-     pdo_query("ALTER TABLE  ".tablename('business')." CHANGE `content` `content` text NOT NULL;");
-}
 if (pdo_fieldexists('business', 'content')) {
-	pdo_query('ALTER TABLE '.tablename('business')." CHANGE `content` `content` mediumtext NOT NULL DEFAULT ''");
+		$showcreatetable = pdo_fetch("SHOW CREATE TABLE" . tablename('business'));
+	$find = strstr($showcreatetable['Create Table'], '`content` mediumtext');
+	if (empty($find)) {
+		pdo_query("ALTER TABLE ".tablename('business')." CHANGE `content` `content` mediumtext NOT NULL DEFAULT ''");
+	}
 }
