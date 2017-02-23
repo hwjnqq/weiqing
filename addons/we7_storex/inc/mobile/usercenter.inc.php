2f738ec77299dd87992156cf5b8dfb0ee0fecb78
yangfeng <117635341@qq.com>
2017-02-23 11:16:49 +0800

    新模块测试

--- addons/we7_storex/inc/mobile/usercenter.inc.php
+++ addons/we7_storex/inc/mobile/usercenter.inc.php
@@ -8,7 +8,8 @@ global $_W, $_GPC;
 $op = trim($_GPC['op']) ? trim($_GPC['op']) : 'display';
 
 if ($op == 'user_credit') {
-	$user_info = pdo_get('mc_members', array('uid' => $_GPC['uid']));
+	// $user_info = pdo_get('mc_members', array('uid' => $_GPC['uid']));
+	$user_info = mc_fetch($_W['openid']);
 	message(error(-1, $user_info), '', 'ajax');
 	// return result(0, '获取酒店成功', $_W['openid']);
 }
