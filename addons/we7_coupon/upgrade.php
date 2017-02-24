9bc71d960251439852beb7bbc59d42cd7b4c7643
yangfeng <117635341@qq.com>
2017-02-24 15:50:38 +0800

    卡券模块升级脚本开启云参数设置

--- addons/we7_coupon/upgrade.php
+++ addons/we7_coupon/upgrade.php
@@ -2,6 +2,6 @@
 
 $we7_coupon = pdo_get('modules', array('name' => 'we7_coupon'));
 if (!empty($we7_coupon)) {
-	pdo_update('modules', array('issystem' => '1'), array('mid' => $we7_coupon['mid']));
+	pdo_update('modules', array('issystem' => '1', 'settings' => '2'), array('mid' => $we7_coupon['mid']));
 }
 
