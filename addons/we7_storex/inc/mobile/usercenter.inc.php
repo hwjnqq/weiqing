7fc7fcab752dbc4136f111c176912f461d1f09c5
xiongfei <13610612377@163.com>
2017-02-27 09:22:37 +0800

    获取个人信息和更改个人信息

--- addons/we7_storex/inc/mobile/usercenter.inc.php
+++ addons/we7_storex/inc/mobile/usercenter.inc.php
@@ -2,6 +2,7 @@
 
 defined('IN_IA') or exit('Access Denied');
 global $_W, $_GPC;
+load()->model('mc');
 // paycenter_check_login();
 // $ops = array('display', 'post', 'delete');
 // $op = in_array($op, $op) ? $op : 'display';
@@ -14,5 +15,19 @@ if ($op == 'user_credit') {
 	// return result(0, '获取酒店成功', $_W['openid']);
 }
 // return result(0, '获取酒店成功', array('1', '2', '3'));
+//获取个人信息
+if ($op == 'personal_info') {
+	$user_information = mc_fetch($_W['openid']);
+	message(error(0, $user_information), '', 'ajax');
+}
+if ($op == 'personal_update'){
+//	$fields数组形式，所要更改的内容
+	$user_update = mc_update($_W['openid'], $fields);
+	message(error(0, $user_update), '', 'ajax');
+//	更改成功返回true，失败返回false
+}
+if ($op == 'info'){
+
+}
 
 include $this->template('usercenter');
