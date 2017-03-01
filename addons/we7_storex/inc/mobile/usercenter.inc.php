da7db771b0225d196c099dbbc2d2015222ca65dc
xiongfei <13610612377@163.com>
2017-03-01 13:47:48 +0800

    微66店个人信息更改

--- addons/we7_storex/inc/mobile/usercenter.inc.php
+++ addons/we7_storex/inc/mobile/usercenter.inc.php
@@ -8,6 +8,7 @@ load()->model('mc');
 // $op = in_array($op, $op) ? $op : 'display';
 $op = trim($_GPC['op']) ? trim($_GPC['op']) : 'display';
 
+$_W['openid'] = 'oTKzFjpkpEKpqXibIshcJLsmeLVo';
 $uid = mc_openid2uid($_W['openid']);
 
 if ($op == 'personal_info') {
@@ -31,30 +32,31 @@ if ($op == 'credits_record'){
 	$condition = 'WHERE uniacid = :uniacid AND credittype = :credittype AND uid = :uid AND module = :module';
 	$params = array(':uniacid' => $_W['uniacid'], ':credittype' => $_GPC['credittype'], ':uid' => $uid, 'module' => 'we7_storex');
 	$credits_record = pdo_fetchall('SELECT num, createtime , module, remark FROM ' .tablename('mc_credits_record') .$condition .' ORDER BY id DESC', $params);
+//	$credits_record = pdo_getall('mc_credits_record', array('uniacid' => $_W['uniacid'], 'credittype' => $_GPC['credittype'], 'uid' => $uid, 'module' => 'we7_storex'), array('num', 'createtime', 'module','remark' ), '', 'id DESC');
 	message(error(0, $credits_record), '', 'ajax');
 }
-if ($op == 'address_info'){
-	$address_info = pdo_getall('store_address', array('uid' => $uid));
+if ($op == 'address_lists'){
+	$address_info = pdo_getall('mc_member_address', array('uid' => $uid));
 	message(error(0,$address_info),'','ajax');
 }
 if ($op == 'post'){
 	$address_id = intval($_GPC['id']);
 	if(!empty($address_id)){
-		$result = pdo_update('store_address', $_GPC['fields'], array('id' => $address_id));
+		$result = pdo_update('mc_member_address', $_GPC['fields'], array('id' => $address_id));
 		message(error(0, $result), '', 'ajax');
 	}else{
 		if(empty($_GPC['fields'])){
 			message(error(-1, 添加数据错误), '', 'ajax');
 		}
-		$result = pdo_insert('store_address', $_GPC['fields']);
+		$result = pdo_insert('mc_member_address', $_GPC['fields']);
 		message(error(0, $result), '', 'ajax');
 	}
-
 }
 if ($op == 'address_delete'){
 	$address_id = intval($_GPC['id']);
-	$result = pdo_delete('store_address', array('id' => $address_id));
+	$result = pdo_delete('mc_member_address', array('id' => $address_id));
 	message(error(0, $result), '', 'ajax');
 }
+
 
 include $this->template('usercenter');
