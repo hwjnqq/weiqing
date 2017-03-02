b5424eca765e61985f23c8a624a21592c1c07a7d
王瑞帅 <13754885770@163.com>
2017-03-02 15:14:00 +0800

    万能小店修改

--- addons/we7_storex/function/function.php
+++ addons/we7_storex/function/function.php
@@ -86,6 +86,9 @@ function check_params($op){
 			'order_detail' => array(
 				'id' => intval($_GPC['id']),
 			),
+			'orderpay' => array(
+				'id' => intval($_GPC['id']),
+			)
 		),
 		'usercenter' => array(
 			'common' => array(
@@ -126,5 +129,128 @@ function check_params($op){
 				}
 			}
 		}	
+	}
+}
+
+//检查条件
+function check_action($action, $goods_info){
+	if (empty($goods_info)) {
+		message(error(-1, '商品未找到, 请联系管理员!'), '', 'ajax');
+	}
+	if($action == 'reserve' && $goods_info['can_reserve'] != 1){
+		message(error(-1, '该商品不能预定'), '', 'ajax');
+	}
+	if($action == 'buy' && $goods_info['can_buy'] != 1){
+		message(error(-1, '该商品不能购买'), '', 'ajax');
+	}
+}
+
+function isMember() {
+	global $_W;
+	//判断公众号是否卡其会员卡功能
+	$card_setting = pdo_fetch("SELECT * FROM ".tablename('mc_card')." WHERE uniacid = '{$_W['uniacid']}'");
+	$card_status =  $card_setting['status'];
+	//查看会员是否开启会员卡功能
+	if($_W['member']['uid']){
+		$membercard_setting  = pdo_get('mc_card_members', array('uniacid' => intval($_W['uniacid']), 'uid' => $_W['member']['uid']));
+		$membercard_status = $membercard_setting['status'];
+		$pricefield = !empty($membercard_status) && $card_status == 1 ? "mprice" : "cprice";
+		if (!empty($card_status) && !empty($membercard_status)) {
+			return true;
+		} else {
+			return false;
+		}
+	}else{
+		return false;
+	}
+}
+
+//action 1预定  2购买
+function check_order_status($item){
+	$order_status_text = array(
+			'1' => '待付款',
+			'2' => '等待店铺确认',
+			'3' => '订单已取消',
+			'4' => '正在退款中',
+			'5' => '待入住',
+			'6' => '店铺已拒绝',
+			'7' => '已退款',
+			'8' => '已入住',
+			'9' => '已完成',
+			'10' => '未发货',
+			'11' => '已发货',
+			'12' => '已收货',
+	);
+	//1是显示,2不显示
+	$item['is_pay'] = 2;//立即付款 is_pay
+	$item['is_cancle'] = 2;//取消订单is_cancle
+	$item['is_confirm'] = 2;//确认收货is_confirm
+	$item['is_over'] = 2;//再来一单is_over
+	if ($item['status'] == 0){
+		if ($item['paystatus']== 0){
+			$status = STORE_UNPAY_STATUS;
+			$item['is_pay'] = 1;
+			$item['is_cancle'] = 1;
+		}else{
+			$status = STORE_SURE_STATUS;
+			$item['is_cancle'] = 1;
+		}
+	}else if ($item['status'] == -1){
+		if ($item['paystatus']== 0){
+			$status = STORE_CANCLE_STATUS;
+			$item['is_over'] = 1;
+		}else{
+			$status = STORE_REPAY_STATUS;
+		}
+	}else if ($item['status'] == 1){
+		if($item['store_type'] == 1){//酒店
+			if ($item['paystatus']== 0){
+				$item['is_pay'] = 1;
+				$item['is_cancle'] = 1;
+				$status = STORE_UNLIVE_STATUS;
+			}else{
+				$item['is_cancle'] = 1;
+				$status = STORE_UNLIVE_STATUS;
+			}
+		}else{//确认订单后显示货物状态，普通
+			if($item['goods_status'] == 1){
+				$status = STORE_UNSENT_STATUS;
+			}elseif($item['goods_status'] == 2){
+				$item['is_confirm'] = 1;
+				$status = STORE_SENT_STATUS;
+			}elseif($item['goods_status'] == 3){
+				$status = STORE_GETGOODS_STATUS;
+			}
+		}
+	}else if ($item['status'] == 2){
+		if ($item['paystatus']== 0){
+			$status = STORE_REFUSE_STATUS;
+		}else{
+			$status = STORE_REPAY_SUCCESS_STATUS;
+		}
+	}else if ($item['status'] == 4){
+		$status = STORE_LIVE_STATUS;
+	}else if ($item['status'] == 3){
+		$status = STORE_OVER_STATUS;
+		$item['over'] = 1;
+	}
+	$item['order_status'] = $order_status_text[$status];
+	return $item;
+}
+
+function pay_info($order_id){
+	global $_W;
+	$order_info = pdo_get('hotel2_order', array('id' => $order_id, 'weid' => intval($_W['uniacid']), 'openid' => $_W['openid']));
+	if(!empty($order_info)){
+		$params = array(
+				'ordersn' => $order_info['ordersn'],
+				'tid' => $order_info['id'],//支付订单编号, 应保证在同一模块内部唯一
+				'title' => $_W['account']['name'] . "店铺订单{$order_info['ordersn']}",//商家名称
+				'fee' => $order_info['sum_price'],//总费用, 只能大于 0
+				'user' => $_W['openid'],//付款用户, 付款的用户名(选填项)
+		);
+		return $params;
+	}else{
+		message(error(-1, '获取订单信息失败'), '', 'ajax');
 	}
 }
