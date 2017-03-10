fa6213530df4baac1b1f14bba2bc88e047506555
王瑞帅 <13754885770@163.com>
2017-03-10 17:22:09 +0800

    万能小店后台修改和评价接口

--- addons/wn_storex/function/function.php
+++ addons/wn_storex/function/function.php
@@ -57,6 +57,9 @@ function check_params(){
 			'confirm_goods' => array(
 				'id' => intval($_GPC['id']),
 			),
+			'order_comment' => array(
+				'id' => intval($_GPC['id']),
+			)
 		),
 		'usercenter' => array(
 			'common' => array(
@@ -237,111 +240,101 @@ function orders_check_status($item){
 	$item['is_cancle'] = 2;//取消订单is_cancle
 	$item['is_confirm'] = 2;//确认收货is_confirm
 	$item['is_over'] = 2;//再来一单is_over
+	$item['is_comment'] = 2;//显示评价is_comment
 	if ($item['status'] == 0){
-		if($item['action'] == 1){
-			if($item['goods_status'] == 1){
-				$status = STORE_UNSENT_STATUS;
-			}elseif($item['goods_status'] == 2){
-				$item['is_confirm'] = 1;
-				$status = STORE_SENT_STATUS;
-			}elseif($item['goods_status'] == 3){
-				$status = STORE_GETGOODS_STATUS;
-			}else{
-				$item['is_cancle'] = 1;
-				$status = STORE_RESERVE_SUCCESS_STATUS;
-			}
-		}else{
-			if ($item['paystatus']== 0){
+		if ($item['action'] == 1){
+			$status = STORE_SURE_STATUS;
+		} else {
+			if ($item['paystatus'] == 0){
 				$status = STORE_UNPAY_STATUS;
 				$item['is_pay'] = 1;
-				$item['is_cancle'] = 1;
-			}else{
+			} else {
 				$status = STORE_SURE_STATUS;
-				$item['is_cancle'] = 1;
 			}
 		}
-	}else if ($item['status'] == -1){
-		if ($item['action'] == 1){
+		$item['is_cancle'] = 1;
+	} else if ($item['status'] == -1){
+		if ($item['paystatus'] == 0){
 			$status = STORE_CANCLE_STATUS;
 			$item['is_over'] = 1;
-		}else{
-			if ($item['paystatus']== 0){
-				$status = STORE_CANCLE_STATUS;
-				$item['is_over'] = 1;
-			}else{
-				$status = STORE_REPAY_STATUS;
-			}
+		} else {
+			$status = STORE_REPAY_STATUS;
 		}
-	}else if ($item['status'] == 1){
+	} else if ($item['status'] == 1){
 		if ($item['store_type'] == 1){//酒店
 			if ($item['action'] == 1){
+				$status = STORE_CONFIRM_STATUS;
 				$item['is_cancle'] = 1;
-				$status = STORE_RESERVE_CONFIRM_STATUS;
-			}else{
-				if ($item['paystatus']== 0){
+			} else {
+				$status = STORE_UNLIVE_STATUS;
+				$item['is_cancle'] = 1;
+				if ($item['paystatus'] == 0){
 					$item['is_pay'] = 1;
-					$item['is_cancle'] = 1;
-					$status = STORE_UNLIVE_STATUS;
-				}else{
-					$item['is_cancle'] = 1;
-					$status = STORE_UNLIVE_STATUS;
 				}
 			}
-		}else{//确认订单后显示货物状态，普通
-			if($item['paystatus'] == 0){
-				if ($item['goods_status'] == 1){
-					if($item['action'] == 2){
-						$item['is_pay'] = 1;
-					}
+		} else {
+			if ($item['action'] == 1 || $item['paystatus'] == 1){//预定
+				if ($item['mode_distribute'] == 1){//自提
 					$item['is_cancle'] = 1;
-					$status = STORE_UNSENT_STATUS;
-				}elseif($item['goods_status'] == 2){
-					if($item['action'] == 2){
-						$item['is_pay'] = 1;
-					}
-					$item['is_confirm'] = 1;
-					$status = STORE_SENT_STATUS;
-				}elseif($item['goods_status'] == 3){
-					if($item['action'] == 2){
-						$item['is_pay'] = 1;
-					}
-					$status = STORE_GETGOODS_STATUS;
-				}else{
-					if($item['action'] == 2){
-						$item['is_pay'] = 1;
-					}
-					$item['is_cancle'] = 1;
-					$status = STORE_RESERVE_CONFIRM_STATUS;
-				}
-			}else if($item['paystatus'] == 1){
-				if($item['mode_distribute'] == 1){//自提
 					$status = STORE_CONFIRM_STATUS;
-				}else{
+				} else if ($item['mode_distribute'] == 2) {
 					if ($item['goods_status'] == 1){
 						$item['is_cancle'] = 1;
 						$status = STORE_UNSENT_STATUS;
-					}elseif($item['goods_status'] == 2){
+					} else if ($item['goods_status'] == 2){
 						$item['is_confirm'] = 1;
 						$status = STORE_SENT_STATUS;
-					}elseif($item['goods_status'] == 3){
+					} else if ($item['goods_status'] == 3){
 						$status = STORE_GETGOODS_STATUS;
+					} else {
+						$item['is_cancle'] = 1;
+						$status = STORE_CONFIRM_STATUS;
 					}
 				}
-			}else{
-				$status = STORE_REPAY_STATUS;
+			} else {
+				if ($item['paystatus'] == 0){
+					if ($item['mode_distribute'] == 1 ){//自提
+						$item['is_cancle'] = 1;
+						$item['is_pay'] = 1;
+						$status = STORE_CONFIRM_STATUS;
+					} else if ($item['mode_distribute'] == 2) {
+						if ($item['goods_status'] == 1){
+							$item['is_cancle'] = 1;
+							$item['is_pay'] = 1;
+							$status = STORE_UNSENT_STATUS;
+						} else if ($item['goods_status'] == 2){
+							$item['is_pay'] = 1;
+							$item['is_confirm'] = 1;
+							$status = STORE_SENT_STATUS;
+						} else if ($item['goods_status'] == 3){
+							$item['is_pay'] = 1;
+							$status = STORE_GETGOODS_STATUS;
+						} else {
+							$item['is_cancle'] = 1;
+							$item['is_pay'] = 1;
+							$status = STORE_CONFIRM_STATUS;
+						}
+					}
+				} else {
+					$status = STORE_REPAY_STATUS;
+				}
 			}
 		}
-	}else if ($item['status'] == 2){
-		if ($item['paystatus']== 0){
+	} else if ($item['status'] == 2){
+		if ($item['paystatus'] == 0){
 			$status = STORE_REFUSE_STATUS;
-		}else{
+		} else {
 			$status = STORE_REPAY_SUCCESS_STATUS;
 		}
-	}else if ($item['status'] == 4){
+	} else if ($item['status'] == 4){
 		$status = STORE_LIVE_STATUS;
-	}else if ($item['status'] == 3){
+		$item['is_over'] = 1;
+	} else if ($item['status'] == 3){
 		$status = STORE_OVER_STATUS;
-		$item['over'] = 1;
+		$item['is_over'] = 1;
+		if ($item['comment'] == 0){
+			$item['is_comment'] = 1;
+		}
 	}
 	$setting = pdo_get('storex_set', array('weid' => intval($_W['uniacid'])));
 	if ($setting['refund'] == 1) {
