ff7a2334a9b12323e6bdae7369a466217a379e4f
yangfeng <117635341@qq.com>
2017-04-07 13:58:48 +0800

    万能小店扩展功能

--- addons/wn_storex/inc/web/extend_membercard.inc.php
+++ addons/wn_storex/inc/web/extend_membercard.inc.php
@@ -4,18 +4,18 @@ defined('IN_IA') or exit('Access Denied');
 
 global $_W, $_GPC;
 $ops = array('card_setting', 'member_list', 'signmanage', 'noticemanage');
-$op = in_array($_GPC['op'], $ops) ? trim($_GPC['op']) : 'card_setting';
+$op = in_array($_GPC['op'], $ops) ? trim($_GPC['op']) : 'signmanage';
 
 if ($op == 'signmanage') {
-	$url = $this->createWebUrl('signmanage');
+	$url = $this->createWebUrl('signmanage', array('op' => 'sign-credit'));
 	header("Location: {$url}");
 	die;
 }
 
 if ($op == 'noticemanage') {
-	$url = $this->createWebUrl('noticemanage');
+	$url = $this->createWebUrl('noticemanage', array('op' => 'list'));
 	header("Location: {$url}");
 	die;
 }
 
-include $this->template('extend_membercard');
+// include $this->template('extend_membercard');
