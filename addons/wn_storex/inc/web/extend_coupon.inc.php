4df54b61b7e45e57ce397627072045740f25c349
王瑞帅 <13754885770@163.com>
2017-05-16 17:40:58 +0800

    万能小店统计提交

--- addons/wn_storex/inc/web/extend_coupon.inc.php
+++ addons/wn_storex/inc/web/extend_coupon.inc.php
@@ -3,7 +3,7 @@
 defined('IN_IA') or exit('Access Denied');
 
 global $_W, $_GPC;
-$ops = array('couponmanage', 'couponconsume', 'couponmarket');
+$ops = array('couponmanage', 'couponconsume', 'couponexchange', 'couponmarket', 'wxcardreply');
 $op = in_array($_GPC['op'], $ops) ? trim($_GPC['op']) : 'couponmanage';
 
 if ($op == 'couponmanage') {
@@ -13,19 +13,25 @@ if ($op == 'couponmanage') {
 }
 
 if ($op == 'couponconsume') {
-	$url = $this->createWebUrl('noticemanage', array('op' => 'notice_list'));
+	$url = $this->createWebUrl('noticemanage', array('op' => 'display'));
 	header("Location: {$url}");
 	die;
 }
 
 if ($op == 'couponexchange') {
-	$url = $this->createWebUrl('couponexchange', array('op' => 'exchange_display'));
+	$url = $this->createWebUrl('couponexchange', array('op' => 'display'));
 	header("Location: {$url}");
 	die;
 }
 
-// if ($op == 'couponmarket') {
-// 	$url = $this->createWebUrl('couponmarket', array('op' => 'list'));
-// 	header("Location: {$url}");
-// 	die;
-// }
+if ($op == 'couponmarket') {
+	$url = $this->createWebUrl('couponmarket', array('op' => 'display'));
+	header("Location: {$url}");
+	die;
+}
+
+if ($op == 'wxcardreply') {
+	$url = $this->createWebUrl('wxcardreply', array('op' => 'display'));
+	header("Location: {$url}");
+	die;
+}
