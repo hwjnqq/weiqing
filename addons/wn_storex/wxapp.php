ef8b26715dcb713b9415c0035ac2838c3c0a0a09
王瑞帅 <13754885770@163.com>
2017-06-05 10:39:18 +0800

    万能小店小程序修改

--- addons/wn_storex/wxapp.php
+++ addons/wn_storex/wxapp.php
@@ -8,107 +8,94 @@ use Qiniu\json_decode;
 defined('IN_IA') or exit('Access Denied');
 
 class Wn_storexModuleWxapp extends WeModuleWxapp {
-	//http://prox.we7.cc/app/index.php?i=281&c=entry&a=wxapp&do=Route&m=wn_storex&
+	//http://prox.we7.cc/app/index.php?i=281&c=entry&a=wxapp&do=Route&m=wn_storex&ac=
 	//获取该公众号下的所有酒店信息
 	public function doPageRoute(){
 		load()->func('communication');
 		global $_GPC, $_W;
+		$ac = $_GPC['ac'];
+		$url_param = $this->actions($ac);
+		$url_param['m'] = $_GPC['m'] ? $_GPC['m'] : 'wn_storex';
+		
 		$params = $_GPC['params'];
-		if (empty($params['do']) || empty($params['op'])) {
-			message(error(-1, '访问失败'), '', 'ajax');
-		}
-		$url_param = array(
-			'm' => $_GPC['m'] ? $_GPC['m'] : 'wn_storex',
-			'do' => $params['do'],
-			'op' => $params['op'],
-		);
-		$this->get_action($params['do'], $params['op']);
 		$params['userid'] = mc_openid2uid($_SESSION['openid']);
 		$this->check_login();
 		$url = murl('entry', $url_param, true, true);
 		$result = ihttp_request($url, $params);
 		exit($result['content']);
 	}
-	function get_action($do, $op) { 
+	function actions($ac) {
 		$actions = array(
-			'category' => array(
-				'category_list',
-				'goods_list',
-				'more_goods',
-				'class',
-				'sub_class',
-			),
-			'clerk' => array(
-				'clerkindex',
-				'order',
-				'order_info',
-				'edit_order',
-				'room',
-				'room_info',
-				'edit_room',
-				'permission_storex',
-			),
-			'coupon' => array(
-				'display',
-				'exchange',
-				'mine',
-				'detail',
-				'publish',
-				'opencard',
-				'addcard',
-			),
-			'goods' => array(
-				'goods_info',
-				'info',
-				'order',
-			),
-			'membercard' => array(
-				'receive_info',
-				'receive_card',
-			),
-			'notice' => array(
-				'notice_list',
-				'read_notice',
-			),
-			'orders' => array(
-				'order_list',
-				'order_detail',
-				'orderpay',
-				'cancel',
-				'confirm_goods',
-				'order_comment',
-			),
-			'recharge' => array(
-				'recharge_add',
-				'recharge_pay',
-				'card_recharge',
-			),
-			'sign' => array(
-				'sign_info',
-				'sign',
-				'remedy_sign',
-				'sign_record',
-			),
-			'store' => array(
-				'store_list',
-				'store_detail',
-				'store_comment',
-			),
-			'usercenter' => array(
-				'personal_info',
-				'personal_update',
-				'credits_record',
-				'address_lists',
-				'current_address',
-				'address_post',
-				'address_default',
-				'address_delete',
-				'extend_switch',
-			),
+			'storeList' => array('do' => 'store', 'op' => 'store_list'),
+			'storeDetail' => array('do' => 'store', 'op' => 'store_detail'),
+			'storeComment' => array('do' => 'store', 'op' => 'store_comment'),
+				
+			'categoryClass' => array('do' => 'category', 'op' => 'class'),
+			'categorySub' => array('do' => 'category', 'op' => 'sub_class'),
+			'moreGoods' => array('do' => 'category', 'op' => 'more_goods'),
+				
+			'goodsInfo' => array('do' => 'goods', 'op' => 'goods_info'),
+			'goodsBuyInfo' => array('do' => 'goods', 'op' => 'info'),
+			'goodsOrder' => array('do' => 'goods', 'op' => 'order'),
+			'creditGoodsList' => array('do' => 'goods', 'op' => 'display'),
+			'creditGoodsDetail' => array('do' => 'goods', 'op' => 'detail'),
+			'creditGoodsExchange' => array('do' => 'goods', 'op' => 'exchange'),
+			'creditGoodsMine' => array('do' => 'goods', 'op' => 'mine'),
+			'creditMineConfirm' => array('do' => 'goods', 'op' => 'confirm'),
+				
+			'getUserInfo' => array('do' => 'usercenter', 'op' => 'personal_info'),
+			'updateUserInfo' => array('do' => 'usercenter', 'op' => 'personal_update'),
+			'addressLists' => array('do' => 'usercenter', 'op' => 'address_lists'),
+			'deleteAddress' => array('do' => 'usercenter', 'op' => 'address_delete'),
+			'defaultAddress' => array('do' => 'usercenter', 'op' => 'address_default'),
+			'addressInfo' => array('do' => 'usercenter', 'op' => 'current_address'),
+			'editAddress' => array('do' => 'usercenter', 'op' => 'address_post'),
+			'userCredits' => array('do' => 'usercenter', 'op' => 'credits_record'),
+			'extend' => array('do' => 'usercenter', 'op' => 'extend_switch'),
+				
+			'orderPay' => array('do' => 'orders', 'op' => 'orderpay'),
+			'orderList' => array('do' => 'orders', 'op' => 'order_list'),
+			'orderInfo' => array('do' => 'orders', 'op' => 'order_detail'),
+			'orderComment' => array('do' => 'orders', 'op' => 'order_comment'),
+			'orderCancel' => array('do' => 'orders', 'op' => 'cancel'),
+			'orderConfirm' => array('do' => 'orders', 'op' => 'confirm_goods'),
+				
+			'cardRecharge' => array('do' => 'recharge', 'op' => 'card_recharge'),
+			'rechargeAdd' => array('do' => 'recharge', 'op' => 'recharge_add'),
+			'rechargePay' => array('do' => 'recharge', 'op' => 'recharge_pay'),
+				
+			'signInfo' => array('do' => 'sign', 'op' => 'sign_info'),
+			'signSing' => array('do' => 'sign', 'op' => 'sign'),
+			'remedySign' => array('do' => 'sign', 'op' => 'remedy_sign'),
+			'signRecord' => array('do' => 'sign', 'op' => 'sign_record'),
+				
+			'noticeList' => array('do' => 'notice', 'op' => 'notice_list'),
+			'noticeRead' => array('do' => 'notice', 'op' => 'read_notice'),
+				
+			'receiveCard' => array('do' => 'membercard', 'op' => 'receive_card'),
+			'receiveInfo' => array('do' => 'membercard', 'op' => 'receive_info'),
+				
+			'couponList' => array('do' => 'coupon', 'op' => 'display'),
+			'couponExchange' => array('do' => 'coupon', 'op' => 'exchange'),
+			'myCoupon' => array('do' => 'coupon', 'op' => 'mine'),
+			'couponInfo' => array('do' => 'coupon', 'op' => 'detail'),
+			'wxAddCard' => array('do' => 'coupon', 'op' => 'addcard'),
+			'wxOpenCard' => array('do' => 'coupon', 'op' => 'opencard'),
+			'creditCouponList' => array('do' => 'coupon', 'op' => 'display'),
+			'creditCouponDetail' => array('do' => 'coupon', 'op' => 'detail'),
+				
+			'clerkPermission' => array('do' => 'clerk', 'op' => 'permission_storex'),
+			'clerkOrder' => array('do' => 'clerk', 'op' => 'order'),
+			'clerkOrderInfo' => array('do' => 'clerk', 'op' => 'order_info'),
+			'clerkOrderEdit' => array('do' => 'clerk', 'op' => 'edit_order'),
+			'clerkRoom' => array('do' => 'clerk', 'op' => 'room'),
+			'clerkRoomInfo' => array('do' => 'clerk', 'op' => 'room_info'),
+			'clerkRoomEdit' => array('do' => 'clerk', 'op' => 'edit_room'),
 		);
-		if (!in_array($op, $actions[$do])) {
-			message(error(-1, '访问失败'), '', 'ajax');
+		if (!empty($actions[$ac])) {
+			return $actions[$ac];
 		}
+		message(error(-1, '访问失败'), '', 'ajax');
 	}
 	public function doPageLocation() {
 		global $_GPC;
