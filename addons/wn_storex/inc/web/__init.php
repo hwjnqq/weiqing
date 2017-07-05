954bda246041d97bc138ac96cde6d593fb24080b
yangfeng <117635341@qq.com>
2017-07-05 10:06:44 +0800

    init修改

--- addons/wn_storex/inc/web/__init.php
+++ addons/wn_storex/inc/web/__init.php
@@ -5,6 +5,9 @@ define('STORE_TYPE_NORMAL', '0');
 define('STORE_TYPE_HOTEL', '1');
 
 if ($_GPC['do'] == 'storebackstage' || $_GPC['do'] == 'goods_category' || $_GPC['do'] == 'goods_manage') {
+	if (empty($_GPC['storeid'])) {
+		message('请重新选择店铺', $this->createWebUrl('storemanage', array('op' => 'list')), 'error');
+	}
 	$storex_bases = pdo_get('storex_bases', array('id' => $_GPC['storeid']));
 	$_W['wn_storex']['store_info'] = $storex_bases;
 }
