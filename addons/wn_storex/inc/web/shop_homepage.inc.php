7a127ad17f9032fbab8a0007d178a50ee099caee
yangfeng <117635341@qq.com>
2017-07-19 10:51:32 +0800

    首页设置

--- addons/wn_storex/inc/web/shop_homepage.inc.php
+++ addons/wn_storex/inc/web/shop_homepage.inc.php
@@ -2,11 +2,70 @@
 defined('IN_IA') or exit('Access Denied');
 
 global $_W, $_GPC;
-$ops = array('display');
+$ops = array('display', 'post');
 $op = in_array(trim($_GPC['op']), $ops) ? trim($_GPC['op']) : 'display';
 
+$storeid = intval($_W['wn_storex']['store_info']['id']);
 if ($op == 'display') {
-	
+	$default_module = array(
+		array(
+			'type' => 'search',
+			'items' => array()
+		),
+		array(
+			'type' => 'slide',
+			'items' => array()
+		),
+		array(
+			'type' => 'notice',
+			'items' => array()
+		),
+		array(
+			'type' => 'nav',
+			'items' => array()
+		),
+		array(
+			'type' => 'cube',
+			'items' => array()
+		),
+		array(
+			'type' => 'nav',
+			'items' => array()
+		),
+		array(
+			'type' => 'recommend',
+			'items' => array()
+		),
+	);
+	$homepage_list = pdo_getall('storex_homepage', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid), array(), 'displayorder', 'displayorder ASC');
+	if (!empty($homepage_list) && is_array($homepage_list)) {
+		 foreach ($homepage_list as $key => &$value) {
+		 	unset($value['id'], $value['displayorder'], $value['uniacid'], $value['storeid']);
+		 	$value['items'] = !empty($value['items']) ? iunserializer($value['items']) : '';
+		 }
+	} else {
+		$homepage_list = $default_module;
+	}
+}
+
+if ($op == 'post') {
+	if ($_W['ispost'] && $_W['isajax']) {
+		$params = $_GPC['params'];
+		if (!empty($params) && is_array($params)) {
+			pdo_delete('storex_homepage', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid));
+			foreach ($params as $key => $value) {
+				$insert = array(
+					'type' => $value['type'],
+					'items' => !empty($value['items']) ? iserializer($value['items']) : '',
+					'displayorder' => $key,
+					'uniacid' => $_W['uniacid'],
+					'storeid' => $storeid,
+				);
+				pdo_insert('storex_homepage', $insert);
+			}
+		}
+		message(error(-1, $insert), '', 'ajax');
+	}
 }
 
 include $this->template('store/shop_homepage');
