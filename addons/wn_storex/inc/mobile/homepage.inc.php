8ec67d9a2a4e0139c41e7230cd31b055a4aed2b8
yangfeng <117635341@qq.com>
2017-07-20 13:40:17 +0800

    手机端首页

---
+++ addons/wn_storex/inc/mobile/homepage.inc.php
@@ -0,0 +1,52 @@
+<?php
+
+defined('IN_IA') or exit('Access Denied');
+
+global $_W, $_GPC;
+
+$ops = array('display');
+$op = in_array(trim($_GPC['op']), $ops) ? trim($_GPC['op']) : 'error';
+
+check_params();
+if ($op == 'display') {
+	$default_module = array(
+		array(
+			'type' => 'search',
+			'items' => array()
+		),
+		array(
+			'type' => 'slide',
+			'items' => array()
+		),
+		array(
+			'type' => 'notice',
+			'items' => array()
+		),
+		array(
+			'type' => 'nav',
+			'items' => array()
+		),
+		array(
+			'type' => 'cube',
+			'items' => array()
+		),
+		array(
+			'type' => 'nav',
+			'items' => array()
+		),
+		array(
+			'type' => 'recommend',
+			'items' => array()
+		),
+	);
+	$homepage_list = pdo_getall('storex_homepage', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid), array(), 'displayorder', 'displayorder ASC');
+	if (!empty($homepage_list) && is_array($homepage_list)) {
+		 foreach ($homepage_list as $key => &$value) {
+		 	unset($value['id'], $value['displayorder'], $value['uniacid'], $value['storeid']);
+		 	$value['items'] = !empty($value['items']) ? iunserializer($value['items']) : '';
+		 }
+	} else {
+		$homepage_list = $default_module;
+	}
+	message(error(0, $homepage_list), '', 'ajax');
+}
