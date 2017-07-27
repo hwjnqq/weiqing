9fda7d4998169c479124d2ca200284b717fd15cf
yangfeng <117635341@qq.com>
2017-07-27 17:55:02 +0800

    回复公众号核销卡券

--- addons/wn_storex/processor.php
+++ addons/wn_storex/processor.php
@@ -10,8 +10,24 @@ defined('IN_IA') or exit('Access Denied');
 
 class Wn_storexModuleProcessor extends WeModuleProcessor {
 
-    public function respond() {
-      
-    }
+	public function respond() {
+		global $_W;
+		$rid = $this->rule;
+		$content = trim($this->message['content']);
+		$code = str_replace('hx', '', $content);
+		$coupon_record = pdo_get('storex_coupon_record', array('uniacid' => $_W['uniacid'], 'openid' => $_W['openid'], 'code' => $code), array('couponid', 'id'));
+		if (empty($coupon_record)) {
+			$message = '未找到该卡券记录';
+		} else {
+			include IA_ROOT . '/addons/wn_storex/model/activity.mod.php';
+			$result = activity_coupon_consume($coupon_record['couponid'], $coupon_record['id'], 0);
+			if (is_error($result)) {
+				$message = $result['message'];
+			} else {
+				$message = '卡券核销成功';
+			}
+		}
 
+		return $this->respText($message);
+	}
 }
