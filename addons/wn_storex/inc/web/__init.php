94e6997a6af4a97a1101af8dd0e824b0d807fe36
yangfeng <117635341@qq.com>
2017-08-01 11:18:01 +0800

    酒店服务插件

--- addons/wn_storex/inc/web/__init.php
+++ addons/wn_storex/inc/web/__init.php
@@ -3,7 +3,7 @@ defined('IN_IA') or exit('Access Denied');
 
 global $_GPC, $_W;
 define('SCRIPT_URL', $_W['siteroot'] . '/addons/wn_storex/template/style/js');
-$dos = array('shop_settings', 'shop_category', 'shop_goodsmanage', 'shop_room_status', 'shop_room_price', 'shop_tagmanage', 'shop_comment', 'shop_index', 'shop_order', 'shop_stat', 'shop_plugin', 'shop_plugin_printer', 'shop_homepage', 'shop_member', 'shop_clerk', 'shop_room_item', 'shop_market', 'shop_memberlevel');
+$dos = array('shop_settings', 'shop_category', 'shop_goodsmanage', 'shop_room_status', 'shop_room_price', 'shop_tagmanage', 'shop_comment', 'shop_index', 'shop_order', 'shop_stat', 'shop_plugin', 'shop_plugin_printer', 'shop_plugin_hotelservice', 'shop_homepage', 'shop_member', 'shop_clerk', 'shop_room_item', 'shop_market', 'shop_memberlevel');
 if (in_array($_GPC['do'], $dos)) {
 	if (empty($_GPC['storeid']) && !($_GPC['do'] == 'shop_settings' && $_GPC['action'] == 'add')) {
 		message('请重新选择店铺', $this->createWebUrl('storemanage', array('op' => 'list')), 'error');
@@ -137,7 +137,8 @@ $aside_nav = array(
 		'icon' => 'storex-menu-app',
 		'active' => array(
 			'shop_plugin',
-			'shop_plugin_printer'
+			'shop_plugin_printer',
+			'shop_plugin_hotelservice'
 		)
 	),
 	'shop_settings' => array(
--- addons/wn_storex/inc/web/shop_plugin.inc.php
+++ addons/wn_storex/inc/web/shop_plugin.inc.php
@@ -5,7 +5,13 @@ global $_W, $_GPC;
 $ops = array('display');
 $op = in_array(trim($_GPC['op']), $ops) ? trim($_GPC['op']) : 'display';
 
+$store_info = $_W['wn_storex']['store_info'];
 if ($op == 'display') {
-
+	$hotel_service_show = false;
+	load()->model('module');
+	$hotel_service = module_fetch('wn_storex_plugin_hotel_service');
+	if (!empty($hotel_service) && $hotel_service['enabled'] == 1) {
+		$hotel_service_show = true;
+	}
 }
 include $this->template('store/shop_plugin');
