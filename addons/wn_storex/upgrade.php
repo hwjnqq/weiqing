d17eda6bce58ee97f0326e003358dc223d5950e3
王瑞帅 <13754885770@163.com>
2017-08-02 15:35:30 +0800

    万能小店版本号修改

--- addons/wn_storex/upgrade.php
+++ addons/wn_storex/upgrade.php
@@ -753,19 +753,19 @@ $js_file_trees = file_tree(IA_ROOT . '/addons/wn_storex/template/style/mobile/js
 $css_file_trees = file_tree(IA_ROOT . '/addons/wn_storex/template/style/mobile/css');
 $svg_file_trees = file_tree(IA_ROOT . '/addons/wn_storex/template/style/mobile/img');
 $current_js_files = array(
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/black.20170724141.js',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/display.20170724141.js',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/manifest.20170724141.js',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/vendor.20170724141.js',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/service.20170724141.js'
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/black.20170731144.js',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/display.20170731144.js',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/manifest.20170731144.js',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/vendor.20170731144.js',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/js/service.20170731144.js'
 );
 $current_css_files = array(
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/black.20170724141.css',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/display.20170724141.css',
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/service.20170724141.css',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/black.20170731144.css',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/display.20170731144.css',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/css/service.20170731144.css',
 );
 $current_svg_files = array(
-	IA_ROOT . '/addons/wn_storex/template/style/mobile/img/storex.20170724141.svg',
+	IA_ROOT . '/addons/wn_storex/template/style/mobile/img/storex.20170731144.svg',
 );
 $css_diff_files = array_diff($css_file_trees, $current_css_files);
 $js_diff_files = array_diff($js_file_trees, $current_js_files);
