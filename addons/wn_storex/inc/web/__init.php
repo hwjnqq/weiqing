2da8dc9b0afa04ce0288513f64f8d6e75f510bb9
yangfeng <117635341@qq.com>
2017-08-05 16:45:07 +0800

    销售员

--- addons/wn_storex/inc/web/__init.php
+++ addons/wn_storex/inc/web/__init.php
@@ -3,7 +3,7 @@ defined('IN_IA') or exit('Access Denied');
 
 global $_GPC, $_W;
 define('SCRIPT_URL', $_W['siteroot'] . '/addons/wn_storex/template/style/js');
-$dos = array('shop_settings', 'shop_category', 'shop_goodsmanage', 'shop_room_status', 'shop_room_price', 'shop_tagmanage', 'shop_comment', 'shop_index', 'shop_order', 'shop_stat', 'shop_plugin', 'shop_plugin_printer', 'shop_plugin_hotelservice', 'shop_homepage', 'shop_member', 'shop_clerk', 'shop_room_item', 'shop_market', 'shop_memberlevel', 'shop_article', 'shop_sales_package');
+$dos = array('shop_settings', 'shop_category', 'shop_goodsmanage', 'shop_room_status', 'shop_room_price', 'shop_tagmanage', 'shop_comment', 'shop_index', 'shop_order', 'shop_stat', 'shop_plugin', 'shop_plugin_printer', 'shop_plugin_hotelservice', 'shop_homepage', 'shop_member', 'shop_clerk', 'shop_room_item', 'shop_market', 'shop_memberlevel', 'shop_article', 'shop_sales_package', 'shop_agent');
 if (in_array($_GPC['do'], $dos)) {
 	if (empty($_GPC['storeid']) && !($_GPC['do'] == 'shop_settings' && $_GPC['action'] == 'add')) {
 		message('请重新选择店铺', $this->createWebUrl('storemanage', array('op' => 'list')), 'error');
@@ -138,7 +138,8 @@ $aside_nav = array(
 		'icon' => 'storex-menu-app',
 		'active' => array(
 			'shop_market',
-			'shop_sales_package'
+			'shop_sales_package',
+			'shop_agent'
 		)
 	),
 	'shop_plugin' => array(
---
+++ addons/wn_storex/inc/web/shop_agent.inc.php
@@ -0,0 +1,37 @@
+<?php
+defined('IN_IA') or exit('Access Denied');
+
+global $_W, $_GPC;
+$ops = array('display', 'agent_info', 'agent_status');
+$op = in_array(trim($_GPC['op']), $ops) ? trim($_GPC['op']) : 'display';
+
+$store_info = $_W['wn_storex']['store_info'];
+$storeid = intval($store_info['id']);
+if ($op == 'display') {
+	$agent_list = pdo_getall('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid));
+	// echo "<pre>";
+	// print_r($agent_list);
+	// echo "</pre>";
+	// exit;
+}
+
+if ($op == 'agent_info') {
+	if ($_W['ispost'] && $_W['isajax']) {
+		$id = intval($_GPC['id']);
+		$agent_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'id' => $id), array('id', 'status', 'reason'));
+		message(error(0, $agent_info), '', 'ajax');
+	}
+}
+
+if ($op == 'agent_status') {
+	if ($_W['ispost'] && $_W['isajax']) {
+		$id = intval($_GPC['id']);
+		$agent_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'id' => $id), array('id'));
+		if (!empty($agent_info)) {
+			pdo_update('storex_agent_apply', array('status' => $_GPC['status'], 'reason' => $_GPC['reason']), array('id' => $id));
+		}
+		message(error(0, ''), referer(), 'ajax');
+	}
+}
+
+include $this->template('store/shop_agent');
