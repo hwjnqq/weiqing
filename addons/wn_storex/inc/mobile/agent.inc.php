663791438bc1cd9eb2c76ce8a89f22da294e1fba
王瑞帅 <13754885770@163.com>
2017-08-10 09:47:46 +0800

    万能小店分销提现修改

--- addons/wn_storex/inc/mobile/agent.inc.php
+++ addons/wn_storex/inc/mobile/agent.inc.php
@@ -13,7 +13,7 @@ $storeid = intval($_GPC['storeid']);
 $uid = mc_openid2uid($_W['openid']);
 
 if ($op == 'display') {
-	$register_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'uid' => $uid));
+	$register_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'uid' => $uid), array('id', 'storeid', 'income', 'outcome', 'alipay', 'level', 'status'));
 	$register_info['status'] = !empty($register_info['status']) ? $register_info['status'] : 4;
 	if ($register_info['status'] == 2) {
 		$agent_level_default = pdo_get('storex_agent_level', array('isdefault' => 1, 'storeid' => $storeid), array('id', 'title'));
@@ -89,37 +89,44 @@ if ($op == 'register') {
 }
 
 if ($op == 'apply') {
-	$register_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'uid' => $uid, 'status' => 2));
-	if (!empty($register_info) && !empty($_GPC['money'])) {
-		if ($register_info['outcome'] == 0) {
-			wmessage(error(-1, '没有钱可以提现'), '', 'ajax');
-		}
-		$money = sprintf('%.2f', $_GPC['money']);
-		if ($money <= 0 || $money > $register_info['outcome']) {
-			wmessage(error(-1, '申请提现金额错误'), '', 'ajax');
-		}
-		$apply_log = array(
-			'uniacid' => $_W['uniacid'],
-			'uid' => $uid,
-			'ordersn' => date('md') . $uid . random(4, 1),
-			'agentid' => $register_info['id'],
-			'storeid' => $storeid,
-			'money' => $money,
-			'time' => TIMESTAMP,
-			'status' => 0,
-		);
-		pdo_insert('storex_agent_apply_log', $apply_log);
-		$apply_log_id = pdo_insertid();
-		if (!empty($apply_log_id)) {
-			$result = pdo_update('storex_agent_apply', array('outcome -=' => $money), array('id' => $register_info['id'], 'uid' => $uid, 'storeid' => $storeid));
-			if (!empty($result)) {
-				wmessage(error(0, '申请提现成功'), '', 'ajax');
-			} else {
-				pdo_delete('storex_agent_apply_log', array('id' => $apply_log_id));
+	$register_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'uid' => $uid, 'status' => 2), array('id', 'outcome', 'income'));
+	if (!empty($register_info)) {
+		if (!empty($_GPC['money'])) {
+			if ($register_info['outcome'] == 0) {
+				wmessage(error(-1, '没有钱可以提现'), '', 'ajax');
 			}
+			$money = sprintf('%.2f', $_GPC['money']);
+			if ($money <= 0 || $money > $register_info['outcome']) {
+				wmessage(error(-1, '申请提现金额错误'), '', 'ajax');
+			}
+			$apply_log = array(
+				'uniacid' => $_W['uniacid'],
+				'uid' => $uid,
+				'ordersn' => date('md') . $uid . random(4, 1),
+				'agentid' => $register_info['id'],
+				'storeid' => $storeid,
+				'money' => $money,
+				'time' => TIMESTAMP,
+				'status' => 0,
+			);
+			pdo_insert('storex_agent_apply_log', $apply_log);
+			$apply_log_id = pdo_insertid();
+			if (!empty($apply_log_id)) {
+				$result = pdo_update('storex_agent_apply', array('outcome -=' => $money), array('id' => $register_info['id'], 'uid' => $uid, 'storeid' => $storeid));
+				if (!empty($result)) {
+					wmessage(error(0, '申请提现成功'), '', 'ajax');
+				} else {
+					pdo_delete('storex_agent_apply_log', array('id' => $apply_log_id));
+					wmessage(error(-1, '申请提现失败'), '', 'ajax');
+				}
+			} else {
+				wmessage(error(-1, '申请提现失败'), '', 'ajax');
+			}
+		} else {
+			wmessage(error(0, $register_info), '', 'ajax');
 		}
 	}
-	wmessage(error(-1, '申请提现失败'), '', 'ajax');
+	wmessage(error(-1, '你还不是销售员'), '', 'ajax');
 }
 
 if ($op == 'apply_list') {
