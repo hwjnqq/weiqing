67d1c54e4b824530ece88643c98a0462a658f9d7
yangfeng <117635341@qq.com>
2017-09-21 15:16:40 +0800

    新菜单修改

--- addons/wn_storex/inc/web/__init.php
+++ addons/wn_storex/inc/web/__init.php
@@ -12,7 +12,7 @@ if (in_array($_GPC['do'], $dos)) {
 	if (empty($_GPC['storeid']) && !($_GPC['do'] == 'shop_settings' && $_GPC['action'] == 'add')) {
 		message('请重新选择店铺', $this->createWebUrl('storemanage', array('op' => 'list')), 'error');
 	}
-	$storex_bases = pdo_get('storex_bases', array('id' => $_GPC['storeid']));
+	$storex_bases = pdo_get('storex_bases', array('id' => 	$_GPC['storeid']));
 	$_W['wn_storex']['store_info'] = $storex_bases;
 	$_W['wn_storex']['table_storeid'] = 'store_base_id';
 	if (empty($_W['wn_storex']['store_info']['store_type'])) {
@@ -50,81 +50,134 @@ if (in_array($_GPC['do'], $dos)) {
 //店铺后台菜单设置
 $aside_nav = array(
 	'shop_index' => array(
-		'title' => '概况',
+		'title' => '店铺管理',
 		'url' => $this->createWebUrl('shop_index', array('storeid' => $_GPC['storeid'])),
 		'icon' => 'storex-menu-index',
 		'active' => array(
 			'shop_index',
-		)
-	),
-	'shop_homepage' => array(
-		'title' => '首页设置',
-		'url' => $this->createWebUrl('shop_homepage', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-home',
-		'active' => array(
-			'shop_homepage'
-		)
-	),
-	'shop_wxapphomepage' => array(
-		'title' => '小程序首页设置',
-		'url' => $this->createWebUrl('shop_wxapphomepage', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-home',
-		'active' => array(
-			'shop_wxapphomepage'
+			'shop_homepage',
+			'shop_wxapphomepage',
+			'shop_stat',
+			'shop_article',
+			'shop_settings',
+			'shop_share'
+		),
+		'children' => array(
+			'shop_index' => array(
+				'title' => '概况',
+				'url' => $this->createWebUrl('shop_index', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-index',
+				'active' => array(
+					'shop_index',
+				)
+			),
+			'shop_homepage' => array(
+				'title' => '首页设置',
+				'url' => $this->createWebUrl('shop_homepage', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-home',
+				'active' => array(
+					'shop_homepage',
+				)
+			),
+			'shop_wxapphomepage' => array(
+				'title' => '小程序首页设置',
+				'url' => $this->createWebUrl('shop_wxapphomepage', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-home',
+				'active' => array(
+					'shop_wxapphomepage',
+				)
+			),
+			'shop_stat' => array(
+				'title' => '数据统计',
+				'url' => $this->createWebUrl('shop_stat', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-stat',
+				'active' => array(
+					'shop_stat',
+				)
+			),
+			'shop_article' => array(
+				'title' => '文章管理',
+				'url' => $this->createWebUrl('shop_article', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-clerk',
+				'active' => array(
+					'shop_article',
+				)
+			),
+			'shop_settings' => array(
+				'title' => '店铺设置',
+				'url' => $this->createWebUrl('shop_settings', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-setting',
+				'active' => array(
+					'shop_settings',
+					'shop_share'
+				)
+			),
 		)
 	),
 	'shop_category' => array(
-		'title' => '商品分类',
+		'title' => '商品管理',
 		'url' => $this->createWebUrl('shop_category', array('storeid' => $_GPC['storeid'])),
 		'icon' => 'storex-menu-category',
 		'active' => array(
 			'shop_category',
-		)
-	),
-	'shop_spec' => array(
-		'title' => '商品规格',
-		'url' => $this->createWebUrl('shop_spec', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-category',
-		'active' => array(
 			'shop_spec',
-			'shop_spec_value'
-		)
-	),
-	'shop_goodsmanage' => array(
-		'title' => '商品管理',
-		'url' => $this->createWebUrl('shop_goodsmanage', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-goods',
-		'active' => array(
 			'shop_goodsmanage',
+			'shop_comment',
+			'shop_spec_value',
 			'shop_room_status',
 			'shop_room_price',
 			'shop_room_item',
 			'shop_tagmanage',
 			'shop_goods_spec'
+		),
+		'children' => array(
+			'shop_category' => array(
+				'title' => '商品分类',
+				'url' => $this->createWebUrl('shop_category', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-category',
+				'active' => array(
+					'shop_category'
+				)
+			),
+			'shop_spec' => array(
+				'title' => '商品规格',
+				'url' => $this->createWebUrl('shop_spec', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-category',
+				'active' => array(
+					'shop_spec',
+					'shop_spec_value'
+				)
+			),
+			'shop_goodsmanage' => array(
+				'title' => '商品管理',
+				'url' => $this->createWebUrl('shop_goodsmanage', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-goods',
+				'active' => array(
+					'shop_goodsmanage',
+					'shop_room_status',
+					'shop_room_price',
+					'shop_room_item',
+					'shop_tagmanage',
+					'shop_goods_spec'
+				)
+			),
+			'shop_comment' => array(
+				'title' => '商品评价',
+				'url' => $this->createWebUrl('shop_comment', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-comment',
+				'active' => array(
+					'shop_comment',
+				)
+			),
 		)
-	),
-	'shop_comment' => array(
-		'title' => '商品评价',
-		'url' => $this->createWebUrl('shop_comment', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-comment',
-		'active' => array(
-			'shop_comment',
-		)
+
 	),
 	'shop_order' => array(
-		'title' => '店铺订单',
+		'title' => '订单管理',
 		'url' => $this->createWebUrl('shop_order', array('storeid' => $_GPC['storeid'])),
 		'icon' => 'storex-menu-order',
 		'active' => array(
 			'shop_order',
-		)
-	),
-	'shop_stat' => array(
-		'title' => '数据统计',
-		'url' => $this->createWebUrl('shop_stat', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-stat',
-		'active' => array(
-			'shop_stat',
 		)
 	),
 	'shop_member' => array(
@@ -133,24 +186,27 @@ $aside_nav = array(
 		'icon' => 'storex-menu-user',
 		'active' => array(
 			'shop_member',
-			'shop_memberlevel',
-		)
-	),
-	'shop_clerk' => array(
-		'title' => '店员管理',
-		'url' => $this->createWebUrl('shop_clerk', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-clerk',
-		'active' => array(
 			'shop_clerk',
-		)
-	),
-	'shop_article' => array(
-		'title' => '文章管理',
-		'url' => $this->createWebUrl('shop_article', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-clerk',
-		'active' => array(
-			'shop_article',
-			'shop_article_category'
+			'shop_memberlevel'
+		),
+		'children' => array(
+			'shop_member' => array(
+				'title' => '用户管理',
+				'url' => $this->createWebUrl('shop_member', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-user',
+				'active' => array(
+					'shop_member',
+					'shop_memberlevel',
+				)
+			),
+			'shop_clerk' => array(
+				'title' => '店员管理',
+				'url' => $this->createWebUrl('shop_clerk', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-clerk',
+				'active' => array(
+					'shop_clerk',
+				)
+			),
 		)
 	),
 	'shop_agent' => array(
@@ -164,30 +220,45 @@ $aside_nav = array(
 		)
 	),
 	'shop_market' => array(
-		'title' => '营销',
+		'title' => '营销活动',
 		'url' => $this->createWebUrl('shop_market', array('storeid' => $_GPC['storeid'])),
 		'icon' => 'storex-menu-app',
 		'active' => array(
 			'shop_market',
-			'shop_sales_package'
-		)
-	),
-	'shop_activity' => array(
-		'title' => '活动',
-		'url' => $this->createWebUrl('shop_activity', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-app',
-		'active' => array(
 			'shop_activity',
-		)
-	),
-	'shop_blast' => array(
-		'title' => '爆客',
-		'url' => $this->createWebUrl('shop_blast', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-app',
-		'active' => array(
 			'shop_blast',
+			'shop_sales_package',
 			'shop_blast_message',
 			'shop_blast_stat'
+		),
+		'children' => array(
+			'shop_market' => array(
+				'title' => '营销活动',
+				'url' => $this->createWebUrl('shop_market', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-app',
+				'active' => array(
+					'shop_market',
+					'shop_sales_package'
+				)
+			),
+			'shop_activity' => array(
+				'title' => '活动',
+				'url' => $this->createWebUrl('shop_activity', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-app',
+				'active' => array(
+					'shop_activity',
+				)
+			),
+			'shop_blast' => array(
+				'title' => '爆客',
+				'url' => $this->createWebUrl('shop_blast', array('storeid' => $_GPC['storeid'])),
+				'icon' => 'storex-menu-app',
+				'active' => array(
+					'shop_blast',
+					'shop_blast_message',
+					'shop_blast_stat'
+				)
+			),
 		)
 	),
 	'shop_plugin' => array(
@@ -200,16 +271,13 @@ $aside_nav = array(
 			'shop_plugin_hotelservice'
 		)
 	),
-	'shop_settings' => array(
-		'title' => '店铺设置',
-		'url' => $this->createWebUrl('shop_settings', array('storeid' => $_GPC['storeid'])),
-		'icon' => 'storex-menu-setting',
-		'active' => array(
-			'shop_settings',
-			'shop_share',
-		)
-	),
 );
+$sub_menu = array();
+foreach ($aside_nav as $key => $value) {
+	if (in_array($_GPC['do'], $value['active'])) {
+		$sub_menu = $value['children'];
+	}
+}
 if ($_W['wn_storex']['store_info']['store_type'] == 1) {
 	unset($aside_nav['shop_agent']);
 	if ($_GPC['do'] == 'shop_agent' || $_GPC['do'] == 'shop_agent_level') {
