b7de39bb12dda9bf42766415fabe58940eed021b
yangfeng <117635341@qq.com>
2017-09-25 16:47:04 +0800

    海报后台添加编辑

--- addons/wn_storex/inc/web/shop_poster.inc.php
+++ addons/wn_storex/inc/web/shop_poster.inc.php
@@ -2,18 +2,56 @@
 defined('IN_IA') or exit('Access Denied');
 
 global $_W, $_GPC;
-$ops = array('display', 'base');
+$ops = array('display', 'post', 'delete');
 $op = in_array(trim($_GPC['op']), $ops) ? trim($_GPC['op']) : 'display';
 
 $storeid = intval($_W['wn_storex']['store_info']['id']);
 $store_info = $_W['wn_storex']['store_info'];
 
 if ($op == 'display') {
-	$pindex = max(1, intval($_GPC['page']));
-	$psize = 20;
-	$clerk_list = pdo_getall('storex_clerk', array('weid' => $_W['uniacid'], 'storeid' => $storeid));
-	$total = pdo_fetchcolumn("SELECT COUNT(*) FROM " . tablename('storex_clerk') . " WHERE weid = :weid AND storeid = :storeid", array(':weid' => $_W['uniacid'], ':storeid' => $storeid));
-	$pager = pagination($total, $pindex, $psize);
+	$poster_list = pdo_getall('storex_poster', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid));
+	include $this->template('store/shop_poster_index');
 }
 
-include $this->template('store/shop_poster');
+if ($op == 'post') {
+	$id = intval($_GPC['id']);
+	$poster_info = pdo_get('storex_poster', array('id' => $id));
+	if (!empty($poster_info)) {
+		$poster_info['list'] = iunserializer($poster_info['params']);
+	} else {
+		$poster_info = array(
+			'type' => 1,
+			'list' => array()
+		);
+	}
+	if ($_W['ispost'] && $_W['isajax']) {
+		$params = $_GPC['params'];
+		$data = array(
+			'name' => $params['name'],
+			'background' => $params['background'],
+			'keyword' => $params['keyword'],
+			'type' => $params['type'],
+			'wait' => $params['wait'],
+			'params' => iserializer($params['list'])
+		);
+		$poster_info = pdo_get('storex_poster', array('id' => $params['id']), array('id'));
+		if (empty($poster_info)) {
+			$data['storeid'] = $storeid;
+			$data['uniacid'] = $_W['uniacid'];
+			pdo_insert('storex_poster', $data);
+		} else {
+			pdo_update('storex_poster', $data, array('id' => $params['id']));
+		}
+		message(error(0, '编辑成功'), $this->createWebUrl('shop_poster', array('op' => 'display', 'storeid' => $storeid)), 'ajax');
+	}
+	include $this->template('store/shop_poster_post');
+}
+
+if ($op == 'delete') {
+	$poster_info = pdo_get('storex_poster', array('id' => $_GPC['id']), array('id'));
+	if (empty($poster_info)) {
+		message('参数错误', '', 'error');
+	}
+	pdo_delete('storex_poster', array('id' => $_GPC['id']));
+	message('删除成功', referer(), 'success');
+}
