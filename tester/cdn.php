441f2f90739ea70534067301a594011928aec6b2
Fanyk <javaflex@yeah.net>
2017-11-11 18:06:59 +0800

    不提交

--- tester/cdn.php
+++ tester/cdn.php
@@ -11,9 +11,12 @@ use Testify\Testify;
 
 require '../framework/bootstrap.inc.php';
 require IA_ROOT . '/framework/library/testify/Testify.php';
-load()->classs('cdn/qiniucdn');
-load()->classs('cdn/cosapi');
-load()->classs('cdn/cos4api');
+load()->library('oss');
+//load()->classs('cdn/qiniuapi');
+//load()->classs('cdn/cosapi');
+//load()->classs('cdn/cos4api');
+//load()->classs('cdn/ossapi');
+load()->classs('remotefile/storage');
 load()->func('communication');
 
 $tester = new Testify('测试CDN');
@@ -34,17 +37,33 @@ $tester->test('cos', function (){
 
 });
 
-//$tester->run();
+$tester->test('oss', function (){
 
+
+});
+
+//$ak = 'LTAItPGnmSk3L5xD';
+//$sk = '2jPGdCxCdnReV8ChP1gdN01oarOVpN';
+//$oos = new OssApi($ak, $sk, 'afantest');
+//$oos->setEndPoint('oss-cn-beijing.aliyuncs.com');
+//
+//var_dump($oos->has('tes12t'));
+
+$content = Storage::disk('oss')->put('dddd', 'cotent');
+var_dump($content);
+
+//$ak = 'AKIDHUEPJRbHDtdIM2gbIdYtskbCfjZUnjGZ';
+//$sk = 'QdPKU6wRGeOYkxiucAzCVqc6uFRl1vvQ';
+//$bucket = 'we7cloud';
+//$appid = '10016060';
+//
+//$cosapi = new Cos4Api($ak, $sk, $bucket, $appid);
+//$cosapi->upload('1234.txt', '123');
+//$oos->delete('tes12t');
 //$ak = 'AKIDsZ2II4FrriMkri8ekZgYAZiaFemeunzy';
 //$sk = 'b0A6pIVS8XgZBXfX9d1GG1WllUyFG1Wq';
 //$bucket = 'cosapi';
 //$appid = '10107262';
 
-$ak = 'AKIDHUEPJRbHDtdIM2gbIdYtskbCfjZUnjGZ';
-$sk = 'QdPKU6wRGeOYkxiucAzCVqc6uFRl1vvQ';
-$bucket = 'we7cloud';
-$appid = '10016060';
 
-$cosapi = new CosApi($ak, $sk, $bucket, $appid);
-$cosapi->upload('123.txt', '123');
+
