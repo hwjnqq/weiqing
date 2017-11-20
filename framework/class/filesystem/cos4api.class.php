040531465ea4a266aaecf1c1d792c1f3e0388a3e
Fanyk <javaflex@yeah.net>
2017-11-20 10:53:58 +0800

    cos4 上传文件

--- framework/class/filesystem/cos4api.class.php
+++ framework/class/filesystem/cos4api.class.php
@@ -6,7 +6,18 @@
  * Time: 15:31
  */
 
-
+/**
+地域	地域简称	默认下载域名	上传域名
+北京一区（华北）	tj	<bucketname>-<APPID>.costj.myqcloud.com	tj.file.myqcloud.com
+北京	bj	<bucketname>-<APPID>.cosbj.myqcloud.com	bj.file.myqcloud.com
+上海（华东）	sh	<bucketname>-<APPID>.cossh.myqcloud.com	sh.file.myqcloud.com
+广州（华南）	gz	<bucketname>-<APPID>.cosgz.myqcloud.com	gz.file.myqcloud.com
+成都（西南）	cd	<bucketname>-<APPID>.coscd.myqcloud.com	cd.file.myqcloud.com
+新加坡	sgp	<bucketname>-<APPID>.cossgp.myqcloud.com	sgp.file.myqcloud.com
+香港	hk	<bucketname>-<APPID>.coshk.myqcloud.com	hk.file.myqcloud.com
+多伦多	ca	<bucketname>-<APPID>.cosca.myqcloud.com	ca.file.myqcloud.com
+法兰克福	ger	<bucketname>-<APPID>.cosger.myqcloud.com	ger.file.myqcloud.com
+ */
 class Cos4Api {
 
 	private $appid = '';
@@ -17,28 +28,40 @@ class Cos4Api {
 	private $currentTime;
 	private $rand ;
 	private $f = '';
+	private $region = '';
 
-	public function __construct($secretid = '', $secretkey = '', $bucket = '', $appid = '') {
+	public function __construct($secretid, $secretkey, $bucket,
+	                            $appid, $region = '') {
 		$this->appid = $appid;
 		$this->secretId = $secretid;
 		$this->secretKey = $secretkey;
 		$this->bucket = $bucket;
 		$this->currentTime = time();
+		$this->region = $region;
 	}
 
-	public function putFile($path, $file) {
-		return $this->put($path, $file);
+	public function setRegion($region) {
+		$this->region = $region;
 	}
 
 	public function put($path, $content) {
+		return $this->upload($path, $content);
+	}
+
+	public function putFile($path, $file) {
+		return $this->upload($path, '@'.$file);
+	}
+
+	protected function upload($path, $content) {
+		$isfile = is_file($content);
 		$path = $this->cosUrlEncode($path);
 		$op = 'upload';
-		$sha = is_file($content) ? sha1_file($content) : sha1($content);
-		$filecontent = is_file($content) ? '@'.$content : $content;
+		$sha = $isfile ? sha1_file($content) : sha1($content);
+		$filecontent = $content;
 		$biz_attr = '';
 		$insertOnly = 1;
-		$post = compact('op','sha', 'filecontent', 'biz_attr', 'insertOnly');
-		return $this->request($path, $post);
+		$post = compact('op', 'filecontent', 'biz_attr', 'insertOnly');
+		return $this->request($path, json_encode($post), !$isfile);
 	}
 
 	public function delete($path) {
@@ -46,12 +69,10 @@ class Cos4Api {
 			'op'=>'delete'
 		);
 		$path = $this->cosUrlEncode($path);
-		return $this->request($path, $post);
+		return $this->request($path, json_encode($post));
 	}
 
-	private function cosUrlEncode($path) {
-		return str_replace('%2F', '/',  rawurlencode($path));
-	}
+
 
 
 	private function sign($path) {
@@ -72,26 +93,22 @@ class Cos4Api {
 		return $sign;
 	}
 
-	private function request($path, $data) {
-		$url = 'http://cd.file.myqcloud.com/files/v2/'.$this->appid.'/'.$this->bucket.$path;
-		$sign = $this->sign($path);
-		return $this->send($url, $data, array('Authorization'=>$sign, 'Content-Type'=>'multipart/form-data'));
+	private function cosUrlEncode($path) {
+		return str_replace('%2F', '/',  rawurlencode($path));
 	}
+
+	private function request($path, $data, $json = true) {
+		$url = "http://$this->region.file.myqcloud.com/files/v2/".$this->appid.'/'.$this->bucket.$path;
+		$sign = $this->sign($path);
+		$contentType = 'application/json';
+		if(!$json) {
+			$contentType = 'mutipart/form';
+		}
+		return $this->send($url, $data, array('Expect'=>'', 'Authorization'=>$sign,  'Content-Type'=> 'application/json'));
+	}
+
 
 	private function send($url, $data, $header) {
 		return ihttp_request($url, $data, $header);
-//		$ch = curl_init();
-//		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-//		curl_setopt($ch, CURLOPT_URL, $url);
-//		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-//		curl_setopt($ch, CURLOPT_POSTFIELDS, $req['data']);
-//		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-//		curl_setopt($ch, CURLOPT_VERBOSE, 1);
-//		$data = curl_exec($ch);
-//		$result = json_decode($data);
-//		var_dump($result); //打印上传信息
-//		curl_close($ch);
-
-		return $result;
 	}
 }
