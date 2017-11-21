78a2271a2e93cc595b7dfabc9e5cb7aaf75f6ea5
wangruishuai <13754885770@163.com>
2017-11-21 11:09:57 +0800

    万能小店分销订单增加分销商显示未完成订单

--- addons/wn_storex/inc/mobile/agent.inc.php
+++ addons/wn_storex/inc/mobile/agent.inc.php
@@ -16,43 +16,124 @@ if ($op == 'display') {
 	$register_info = pdo_get('storex_agent_apply', array('uniacid' => $_W['uniacid'], 'storeid' => $storeid, 'uid' => $uid), array('id', 'storeid', 'income', 'outcome', 'alipay', 'level', 'status', 'realname', 'tel', 'reason'));
 	$register_info['status'] = !empty($register_info['status']) ? $register_info['status'] : 4;
 	if ($register_info['status'] == 2) {
-		$agent_level_default = pdo_get('storex_agent_level', array('isdefault' => 1, 'storeid' => $storeid), array('id', 'title'));
-		if (!empty($register_info['level'])) {
-			$agent_level = pdo_get('storex_agent_level', array('id' => $register_info['level']), array('id', 'title'));
-			if (!empty($agent_level)) {
-				$register_info['level_title'] = $agent_level['title'];
-			} elseif (!empty($agent_level_default)) {
-				$register_info['level_title'] = $agent_level_default['title'];
+		//自己分销的订单
+		$self_orders = pdo_getall('storex_order', array('agentid' => $register_info['id'], 'status' => array(0, 1, 3)), array('id', 'roomid', 'sum_price', 'time', 'nums', 'is_package', 'cart', 'agentid', 'cprice', 'status'), '', 'time DESC');
+		$agent_log = order_goods_infos($self_orders, 1);
+		$two_agent = pdo_getall('storex_agent_apply', array('pid' => $register_info['id']), array('id'));
+		if (!empty($two_agent)) {
+			//二级分销的订单
+			$two_order = pdo_getall('storex_order', array('agentid' => $two_agent, 'status' => array(0, 1, 3)), array('id', 'roomid', 'sum_price', 'time', 'nums', 'is_package', 'cart', 'agentid', 'cprice', 'status'), '', 'time DESC');
+			if (!empty($two_order)) {
+				$two_order_info = order_goods_infos($two_order, 2);
+				$agent_log = array_merge($agent_log, $two_order_info);
 			}
-		} else {
-			if (!empty($agent_level_default)) {
-				$register_info['level_title'] = $agent_level_default['title'];
-			}
-		}
-		$agent_log = pdo_getall('storex_agent_log', array('uniacid' => intval($_W['uniacid']), 'storeid' => $storeid, 'agentid' => $register_info['id']), array('goodid', 'money', 'rate', 'sumprice', 'time'), '', 'time DESC');
-		if (!empty($agent_log) && is_array($agent_log)) {
-			$store = get_store_info($storeid);
-			$table = gettablebytype($store['store_type']);
-			$goodids = array();
-			foreach ($agent_log as $info) {
-				$goodids[] = $info['goodid'];
-			}
-			$goods = pdo_getall($table, array('id' => $goodids), array('id', 'title', 'thumb'), 'id');
-			foreach ($agent_log as $k => &$agent_info) {
-				if (!empty($goods[$agent_info['goodid']])) {
-					if (!empty($goods[$agent_info['goodid']]['thumb'])) {
-						$goods[$agent_info['goodid']]['thumb'] = tomedia($goods[$agent_info['goodid']]['thumb']);
-					}
-					$agent_info['goodsinfo'] = $goods[$agent_info['goodid']];
-				} else {
-					unset($agent_log[$k]);
+			$three_agent = pdo_getall('storex_agent_apply', array('pid' => $two_agent), array('id'));
+			if ($three_agent) {
+				//三级分销的订单
+				$three_order = pdo_getall('storex_order', array('agentid' => $three_agent, 'status' => array(0, 1, 3)), array('id', 'roomid', 'sum_price', 'time', 'nums', 'is_package', 'cart', 'agentid', 'cprice', 'status'), '', 'time DESC');
+				if (!empty($three_order)) {
+					$three_order_info = order_goods_infos($three_order, 3);
+					$agent_log = array_merge($agent_log, $three_order_info);
 				}
 			}
-			unset($agent_info);
 		}
 		$register_info['agent_log'] = $agent_log;
 	}
 	wmessage(error(0, $register_info), '', 'ajax');
+}
+
+function order_goods_infos($orders, $level) {
+	$agent_logs = array();
+	if (!empty($orders) && is_array($orders)) {
+		$goods = array();
+		$good_package_ids = array();
+		foreach ($orders as &$order) {
+			if (!empty($order['cart'])) {
+				$order['cart'] = iunserializer($order['cart']);
+				foreach ($order['cart'] as $g) {
+					$goods[$g['good']['id']] = array(
+						'gid' => $g['good']['id'],
+						'type' => $g['good']['is_package'],
+						'cprice' => $g['good']['cprice'],
+						'nums' => $g['good']['buynums'],
+					);
+				}
+			} elseif (!empty($order['roomid']) && $order['is_package'] == 1) {
+				$goods[$order['roomid']] = array('gid' => $order['roomid'], 'type' => 1, 'cprice' => $order['cprice'], 'nums' => $order['nums']);
+			} elseif (!empty($order['roomid']) && $order['is_package'] == 2) {
+				$good_package_ids[$order['roomid']] = array('gid' => $order['roomid'], 'type' => 2, 'cprice' => $order['cprice'], 'nums' => $order['nums']);
+			}
+		}
+		unset($order);
+		$good_package_infos = pdo_getall('storex_sales_package', array('id' => array_keys($good_package_ids)), array('id', 'agent_ratio', 'thumb', 'title'), 'id');//套餐返给销售员的比例
+		$goods_infos = pdo_getall('storex_goods', array('id' => array_keys($goods)), array('id', 'agent_ratio', 'thumb', 'title'), 'id');//返给销售员的比例
+		foreach ($orders as $order) {
+			if (!empty($order['cart'])) {
+				foreach ($order['cart'] as $g) {
+					if (!empty($goods_infos[$g['good']['id']])) {
+						$agent_ratio = iunserializer($goods_infos[$g['good']['id']]['agent_ratio']);
+						if (empty($agent_ratio[$level]) || $agent_ratio[$level] == 0.00) {
+							continue;
+						}
+						$agent_logs[] = array(
+							'goodid' => $g['good']['id'],
+							'money' => sprintf('%.2f', $g['good']['cprice'] * $g['good']['buynums'] * $agent_ratio[$level] * 0.01),
+							'rate' => $agent_ratio[$level],
+							'sumprice' => $order['sum_price'],
+							'time' => $order['time'],
+							'status' => $order['status'],
+							'goodsinfo' => array(
+								'id' =>	$g['good']['id'],
+								'title' => $goods_infos[$g['good']['id']]['title'],
+								'thumb'	=> tomedia($goods_infos[$g['good']['id']]['thumb']),
+							),
+						);
+					}
+				}
+			} elseif (!empty($order['roomid']) && $order['is_package'] == 1) {
+				if (!empty($goods_infos[$order['roomid']])) {
+					$agent_ratio = iunserializer($goods_infos[$order['roomid']]['agent_ratio']);
+					if (empty($agent_ratio[$level]) || $agent_ratio[$level] == 0.00) {
+						continue;
+					}
+					$agent_logs[] = array(
+						'goodid' => $order['roomid'],
+						'money' => sprintf('%.2f', $order['cprice'] * $order['nums'] * $agent_ratio[$level] * 0.01),
+						'rate' => $agent_ratio[$level],
+						'sumprice' => $order['sum_price'],
+						'time' => $order['time'],
+						'status' => $order['status'],
+						'goodsinfo' => array(
+							'id' =>	$order['roomid'],
+							'title' => $goods_infos[$order['roomid']]['title'],
+							'thumb'	=> tomedia($goods_infos[$order['roomid']]['thumb']),
+						),
+					);
+				}
+			} elseif (!empty($order['roomid']) && $order['is_package'] == 2) {
+				if (!empty($good_package_infos[$order['roomid']])) {
+					$agent_ratio = iunserializer($good_package_infos[$order['roomid']]['agent_ratio']);
+					if (empty($agent_ratio[$level]) || $agent_ratio[$level] == 0.00) {
+						continue;
+					}
+					$agent_logs[] = array(
+						'goodid' => $order['roomid'],
+						'money' => sprintf('%.2f', $order['cprice'] * $order['nums'] * $agent_ratio[$level] * 0.01),
+						'rate' => $agent_ratio[$level],
+						'sumprice' => $order['sum_price'],
+						'time' => $order['time'],
+						'status' => $order['status'],
+						'goodsinfo' => array(
+							'id' =>	$order['roomid'],
+							'title' => $good_package_infos[$order['roomid']]['title'],
+							'thumb'	=> tomedia($good_package_infos[$order['roomid']]['thumb']),
+						),
+					);
+				}
+			}
+		}
+	}
+	return $agent_logs;
 }
 
 if ($op == 'register') {
