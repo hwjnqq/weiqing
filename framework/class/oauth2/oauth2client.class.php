7f46bbd7e2ec92063a1cf2f5dff18ce4a7f1ce36
malina <1643827412@qq.com>
2017-12-06 11:28:32 +0800

    第三方  代码优化

--- framework/class/oauth2/oauth2client.class.php
+++ framework/class/oauth2/oauth2client.class.php
@@ -7,10 +7,26 @@ abstract class OAuth2Client {
 	protected $ak;
 	protected $sk;
 	protected $login_type;
+	protected $stateParam = array(
+		'state' => '',
+		'from' => '',
+		'mode' => ''
+	);
 
 	public function __construct($ak, $sk) {
 		$this->ak = $ak;
 		$this->sk = $sk;
+	}
+
+	public function stateParam() {
+		global $_W;
+		$this->stateParam['state'] = $_W['token'];
+		if (!empty($_W['user'])) {
+			$this->stateParam['mode'] = 'bind';
+		} else {
+			$this->stateParam['mode'] = 'login';
+		}
+		return base64_encode(http_build_query($this->stateParam, '', '&'));
 	}
 
 	public function getLoginType($login_type) {
