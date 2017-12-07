ccbb05612510f93af7e394edff4ff1e3c7fc2834
yangfeng <117635341@qq.com>
2017-12-08 06:49:43 +0800

    delete useless code

--- addons/wn_storex_plugin_credit_mall/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 万能小店
- *
- * @author WeEngine Team & ewei
- * @url
- */
-defined('IN_IA') or exit('Access Denied');
--- addons/wn_storex_plugin_group/upgrade.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!pdo_fieldexists('storex_order', 'group_goodsid')) {
-	pdo_query("ALTER TABLE " . tablename('storex_order') . " ADD `group_goodsid` INT(11) NOT NULL COMMENT '拼团商品设置的id';");
-}
-if (!pdo_fieldexists('storex_order', 'group_id')) {
-	pdo_query("ALTER TABLE " . tablename('storex_order') . " ADD `group_id` INT(11) NOT NULL COMMENT '开团后的id';");
-}
