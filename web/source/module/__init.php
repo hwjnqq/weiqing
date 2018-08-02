70b51a04b45cd86f37d3b6c9dc341c048ad17cb8
brjun <brj1001@163.com>
2018-08-02 19:44:27 +0800

    新增createByUniacid()方法

--- web/source/module/__init.php
+++ web/source/module/__init.php
@@ -6,16 +6,17 @@
 defined('IN_IA') or exit('Access Denied');
 
 if (in_array($action, array('permission', 'default-entry', 'manage-account'))) {
-	define('FRAME', 'account');
 	$referer = (url_params(referer()));
 	if (empty($_GPC['version_id']) && intval($referer['version_id']) > 0) {
 		itoast('', $_W['siteurl'] . '&version_id=' . $referer['version_id']);
 	}
-	$account_api = WeAccount::create();
+	$account_api = WeAccount::createByUniacid($_W['uniacid']);
 	if (is_error($account_api)) {
 		itoast('', url('module/display'));
 	}
 	$check_manange = $account_api->checkIntoManage();
+	//模块内定死使用account
+	define('FRAME', 'account');
 	if (is_error($check_manange)) {
 		$account_display_url = $account_api->accountDisplayUrl();
 		itoast('', $account_display_url);
@@ -25,11 +26,16 @@ if (in_array($action, array('group', 'manage-system'))) {
 	define('FRAME', 'system');
 }
 
-$account_param = WeAccount::createByType($_GPC['account_type']);
-define('ACCOUNT_TYPE', $account_param->type);
-define('ACCOUNT_TYPE_TEMPLATE', $account_param->typeTempalte);
+if (in_array($action, array('display'))) {
+	define('FRAME', '');
+}
 
+$account_base = WeAccount::createByType($_GPC['account_type']);
+define('ACCOUNT_TYPE', $account_base->type);
+define('ACCOUNT_TYPE_TEMPLATE', $account_base->typeTempalte);
+
+$module_all_support = module_support_type();
+$module_support = !empty($module_all_support[$_GPC['support']]) ? $module_all_support[$_GPC['support']]['type'] : ACCOUNT_TYPE_SIGN;
+$module_support_name = $_GPC['support'];
 
 	$_GPC['account_type'] = !empty($_GPC['account_type']) ? $_GPC['account_type'] : ACCOUNT_TYPE_OFFCIAL_NORMAL;
-
-
